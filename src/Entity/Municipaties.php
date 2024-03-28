4753327f48b3d27c55ebe6f3dbf932ed519cd937
Zeineb2 <101104755+Zeineb2@users.noreply.github.com>
2024-03-28 03:36:38 +0100

    Add files via upload

---
+++ src/Entity/Municipaties.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MunicipatiesRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MunicipatiesRepository::class)]
+class Municipaties
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $idMuni = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nomMuni = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $adresseMuni = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $etatMuni = null;
+
+    #[ORM\Column]
+    private ?float $ratingMuni = null;
+
+
+#[ORM\OneToMany(targetEntity: Ressources::class, mappedBy: 'idMuni')]
+private $ressources;
+
+public function __construct()
+{
+    $this->ressources = new ArrayCollection();
+}
+
+
+    public function getIdMuni(): ?int
+    {
+        return $this->idMuni;
+    }
+
+    public function getNomMuni(): ?string
+    {
+        return $this->nomMuni;
+    }
+
+    public function setNomMuni(string $nomMuni)
+    {
+        $this->nomMuni = $nomMuni;
+
+        return $this;
+    }
+
+    public function getAdresseMuni(): ?string
+    {
+        return $this->adresseMuni;
+    }
+
+    public function setAdresseMuni(string $adresseMuni)
+    {
+        $this->adresseMuni = $adresseMuni;
+
+        return $this;
+    }
+
+    public function getEtatMuni(): ?string
+    {
+        return $this->etatMuni;
+    }
+
+    public function setEtatMuni(string $etatMuni)
+    {
+        $this->etatMuni = $etatMuni;
+
+        return $this;
+    }
+
+    public function getRatingMuni(): ?float
+    {
+        return $this->ratingMuni;
+    }
+
+    public function setRatingMuni(float $ratingMuni)
+    {
+        $this->ratingMuni = $ratingMuni;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Ressources>
+     */
+    public function getRessources(): Collection
+    {
+        return $this->ressources;
+    }
+
+    public function addRessource(Ressources $ressource): static
+    {
+        if (!$this->ressources->contains($ressource)) {
+            $this->ressources->add($ressource);
+            $ressource->setIdMuni($this);
+        }
+
+        return $this;
+    }
+
+    public function removeRessource(Ressources $ressource): static
+    {
+        if ($this->ressources->removeElement($ressource)) {
+            // set the owning side to null (unless already changed)
+            if ($ressource->getIdMuni() === $this) {
+                $ressource->setIdMuni(null);
+            }
+        }
+
+        return $this;
+    }
+
+
+}
---
+++ src/Entity/Ressources.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\RessourcesRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: RessourcesRepository::class)]
+
+class Ressources
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $idRessource = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nomRessource = null;
+
+    #[ORM\Column]
+    private ?int $nbrRessource = null;
+
+
+    #[ORM\Column]
+    private ?int $nbrDispoRessource = null;
+
+
+    #[ORM\Column(length: 255)]
+    private ?string $imgRessource = null;
+
+
+    #[ORM\ManyToOne(targetEntity: Municipaties::class, inversedBy: 'ressources')]
+    private ?Municipaties $idMuni = null;
+    
+    public function getIdRessource(): ?int
+    {
+        return $this->idRessource;
+    }
+
+    public function getNomRessource(): ?string
+    {
+        return $this->nomRessource;
+    }
+
+    public function setNomRessource(string $nomRessource)
+    {
+        $this->nomRessource = $nomRessource;
+
+        return $this;
+    }
+
+    public function getNbrRessource(): ?int
+    {
+        return $this->nbrRessource;
+    }
+
+    public function setNbrRessource(int $nbrRessource)
+    {
+        $this->nbrRessource = $nbrRessource;
+
+        return $this;
+    }
+
+    public function getNbrDispoRessource(): ?int
+    {
+        return $this->nbrDispoRessource;
+    }
+
+    public function setNbrDispoRessource(int $nbrDispoRessource)
+    {
+        $this->nbrDispoRessource = $nbrDispoRessource;
+
+        return $this;
+    }
+
+    public function getImgRessource(): ?string
+    {
+        return $this->imgRessource;
+    }
+
+    public function setImgRessource(string $imgRessource)
+    {
+        $this->imgRessource = $imgRessource;
+
+        return $this;
+    }
+
+    public function getIdMuni(): ?Municipaties
+    {
+        return $this->idMuni;
+    }
+
+    public function setIdMuni(?Municipaties $idMuni)
+    {
+        $this->idMuni = $idMuni;
+
+        return $this;
+    }
+
+
+}
