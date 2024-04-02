18007a712d7b962752155179e8b62863d2e98b2b
kolsiah <101052716+kolsiah@users.noreply.github.com>
2024-04-03 00:11:28 +0100

    forms

---
+++ src/Form/FormIdeeType.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Idee;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormIdeeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('typeIdee')
+            ->add('descriptionIdee')
+            ->add('imageIdee')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Idee::class,
+        ]);
+    }
+}
---
+++ src/Form/FormSolType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Solutions;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FormSolType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('descriptionSol')
+            ->add('datedSol')
+            ->add('datefSol')
+            ->add('etatSol')
+            ->add('budgetSol')
+            ->add('idRec')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Solutions::class,
+        ]);
+    }
+}
