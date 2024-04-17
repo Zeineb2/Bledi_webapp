4d116888afcc0e0c54aafb280005b973ca0d7c6f
kolsiah <101052716+kolsiah@users.noreply.github.com>
2024-04-17 21:02:56 +0100

    some changes

--- src/Entity/Idee.php
+++ src/Entity/Idee.php
@@ -3,7 +3,7 @@
 namespace App\Entity;
 use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
-
+use App\Repository\IdeeRepository;
 
 #[ORM\Entity(repositoryClass:IdeeRepository::class)]
 
